a836ce84e3b26c2d6fa33d5c9eacaeb7fc630820
Ahmed Raza <razaplus360@gmail.com>
2026-04-24 22:40:42 +0500

    react initialization via vite

---
+++ React Class-01/src/App.jsx
@@ -0,0 +1,41 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <h1>Hello</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+export const App = () => {
+  return (
+    <h1 className="bg-amber-400">Hello Saylani</h1>
+
+  )
+};
+
+
+// export const App = () => {
+//   return (
+//     <h1 className="bg-amber-400">Hello Saylani</h1>
+
+//   )
+// };
+// import {App} from './App.jsx'
+
+
+
+// export default App;
+// import App from './App.jsx'
+
+export {App}
