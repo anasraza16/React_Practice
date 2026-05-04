98e6e04abd420a9d4bf33a8cb96f789eb329c82f
Ahmed Raza <razaplus360@gmail.com>
2026-05-04 18:00:30 +0500

    added class 4

---
+++ React Class-04/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// const [count, setCount] = useState(0)
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className='border border-red-600 flex'>
+      <p> Label</p>
+      <input type='text' className='border-1   '/>
+    </div>
+  );
+};
+
+export default App
